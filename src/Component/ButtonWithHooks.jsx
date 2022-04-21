d019fc58e8724e24265c581211476a49b0454a4e
BuczkoBalazs <buci85@gmail.com>
2022-04-21 16:21:57 +0200

    fetch

--- src/Component/ButtonWithHooks.jsx
+++ src/Component/ButtonWithHooks.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function ButtonWithHooks() {
     const [key1, setKey1] = useState("This is default state of function component");
     const [key2, setKey2] = useState(0);
+    const [countries, setCountries] = useState([]);
+
+    
+    useEffect(
+        () => { 
+            console.log("render")
+            fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(countriesData => setCountries(countriesData))
+        
+        },
+        []
+    )
 
     return (
         <div>
             {key1}
             <button onClick={() => setKey1("This is no default, mon!")}>Lick me Hooks!</button>
             <button onClick={() => setKey2(1)}>{key2}</button>
+            <div className='countries'>
+                {countries.map((country, index) => <div key={index}>{country.name.common}</div>)}
+            </div>
         </div>
     );
 }
