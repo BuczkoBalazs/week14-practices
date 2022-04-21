cc76992e127a156d463e19f5b0cf486f8d2d64e2
BuczkoBalazs <buci85@gmail.com>
2022-04-21 16:03:56 +0200

    Meathook

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header to="This is the end of the Header, mon!"/> 
-    <ButtonWithClass />
-    {/* <ButtonWithHooks />   */}
+    {/* <ButtonWithClass /> */}
+    <ButtonWithHooks />  
   </React.StrictMode>
 );
 
